1232c23012f8b9768396b0da258f064a4fb35a59
Akbar Ardiansah <gooms.gusto@gmail.com>
2025-11-13 13:58:40 +0700

    Create SELECT TEST MAP.sql

---
+++ 001. Multiwarehouse/SELECT TEST MAP.sql
@@ -0,0 +1,3 @@
+﻿SELECT doh.warehouseId,doh.orderNo,doh.soReference1,doh.hedi12, doh.hedi13 FROM DOC_ORDER_HEADER doh WHERE doh.organizationId='OJV_CML' AND doh.customerId='PT.ABC'
+  AND doh.warehouseId='CBT02' AND doh.orderType='SO' 
+  ORDER BY doh.addTime DESC LIMIT 7;
