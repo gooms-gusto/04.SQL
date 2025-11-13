4785258291cb1feb7ac338f0a75d190d0341ed6a
Akbar Ardiansah <gooms.gusto@gmail.com>
2025-11-13 20:58:30 +0700

    Create GetASNProcecable.sql

---
+++ 001. Multiwarehouse/GetASNProcecable.sql
@@ -0,0 +1,13 @@
+﻿SELECT DISTINCT dah.asnReference1
+FROM DOC_ASN_HEADER dah
+WHERE dah.organizationId = @organizationId
+AND dah.warehouseId = @warehouseIdFrom
+AND dah.customerId = @customerId
+AND ((dah.hedi08 = ''
+AND dah.hedi09 = '') OR 
+ (dah.hedi08 IS NULL
+AND dah.hedi09 is NULL))
+AND dah.asnStatus IN ('99')
+AND DATE(doh.addTime) > '2025-10-10'
+ORDER BY dah.asnReference1
+LIMIT @limit;
