8140175ae52e2a5cee02517d5f736edfb51b91ec
Akbar Ardiansah <gooms.gusto@gmail.com>
2025-11-20 07:40:15 +0700

    Add SQL scripts for MAP sortir and job storage
    Added CARA HITUNG MAP SORTIR.sql and JOB_STORAGE.sql scripts along with their respective .design files. These scripts provide queries and event scheduling for MAP sortir calculations and job storage processing in the wms_cml database.

---
+++ CARA HITUNG MAP SORTIR.sql
@@ -0,0 +1,51 @@
+﻿
+
+SELECT cbm.organizationId,cbm.warehouseId,cbm.customerId,SUM(qty),SUM(cbm.total_cbm) *cbm.rate, cbm.stockDate FROM
+  (SELECT
+  ziba.organizationId,
+  ziba.warehouseId,
+  ziba.customerId,
+  ziba.sortirId,
+  ziba.sku,
+  ziba.qtyEach AS qty,
+  IFNULL(BS.`cube`, 0) AS cbm_persku,
+  ziba.qtyEach * IFNULL(BS.`cube`, 0) AS total_cbm,
+  ziba.stockDate,
+  btr.rate
+FROM Z_InventoryBalance_preASN ziba
+  INNER JOIN Z_SORTIR_INBOUND_HEADER zsih
+    ON ziba.organizationId = zsih.organizationId
+    AND ziba.warehouseId = zsih.warehouseId
+    AND ziba.sortirId = zsih.sortirId
+    AND ziba.customerId = zsih.customerId
+  INNER JOIN BAS_SKU BS
+    ON ziba.organizationId = BS.organizationId
+    AND ziba.customerId = BS.customerId
+    AND ziba.sku = BS.sku
+  INNER JOIN BIL_TARIFF_MASTER btm
+    ON ziba.organizationId = btm.organizationId
+    AND ziba.customerId = btm.customerId
+  INNER JOIN BIL_TARIFF_HEADER bth
+    ON bth.organizationId = btm.organizationId
+    AND bth.tariffMasterId = btm.tariffMasterId
+    AND bth.warehouseId = zsih.warehouseId
+  INNER JOIN BIL_TARIFF_DETAILS btd
+    ON btd.organizationId = bth.organizationId
+    AND btd.tariffId = bth.tariffId
+  INNER JOIN BIL_TARIFF_RATE btr
+    ON btr.organizationId = btd.organizationId
+    AND btr.tariffId = btd.tariffId
+    AND btr.tariffLineNo = btd.tariffLineNo
+WHERE ziba.organizationId = 'OJV_CML'
+AND ziba.warehouseId IN ('CBT02', 'JBK01')
+-- AND ziba.customerId IN ('MAP')
+AND ziba.stockDate >= DATE(zsih.addTime)
+AND ziba.stockDate < DATE(IFNULL(zsih.completedTime, NOW()))
+-- AND IFNULL(BS.`cube`,0) = 0;
+AND bth.effectiveFrom <= DATE_FORMAT(CURDATE(), '%Y-%m-%d')
+AND bth.effectiveTo >= DATE_FORMAT(CURDATE(), '%Y-%m-%d')
+AND btd.chargeCategory = 'IV'
+AND btr.rate > 0
+AND bth.tariffMasterId NOT IN ('BIL00062PT')) cbm
+GROUP BY cbm.organizationId,cbm.warehouseId,cbm.customerId,cbm.stockDate,cbm.rate
+-- AND ziba.sortirId='STR0000001251108'
---
+++ JOB_STORAGE.sql
@@ -0,0 +1,34 @@
+﻿use wms_cml;
+
+SELECT zbcc.organizationId,zbcc.warehouseId,zbcc.customerId,zbcc.udf01 AS cronday,zbccd.spName,zbccd.lottable03 AS period
+  FROM Z_BAS_CUSTOMER_CUSTBILLING zbcc
+  INNER JOIN Z_BAS_CUSTOMER_CUSTBILLING_DETAILS zbccd
+  ON zbcc.organizationId = zbccd.organizationId
+  AND zbcc.lotatt01=zbccd.idGroupSp
+  WHERE zbcc.organizationId='OJV_CML'
+  AND IFNULL(zbcc.udf01,'') <> '' AND zbccd.lottable02 = 'no.sql' ; 
+
+
+
+SELECT * FROM Z_BAS_CUSTOMER_CUSTBILLING zbcc;
+
+
+SELECT * FROM Z_BAS_CUSTOMER_CUSTBILLING_DETAILS zbccd;
+
+
+-- Ensure the MySQL Event Scheduler is ON (one-time setup)
+SET GLOBAL event_scheduler = ON;
+
+-- Akbar IT-WH 2025
+DELIMITER $$ 
+CREATE EVENT `CML_Event_ProcessBillingCML_STORAGE`
+ON SCHEDULE EVERY 1 HOUR
+STARTS CURRENT_TIMESTAMP
+DO
+  BEGIN
+    CALL CML_BIL_STORAGE_JOB_NW();
+  END$$ 
+DELIMITER ;
+
+
+SELECT * FROM Z_JOB_EXECUTION_STORAGE_LOG;
